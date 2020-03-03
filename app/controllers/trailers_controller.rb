c91691f86edd326c6e9b9e12d742c7b9dba2039a
reddevilcero <reddevil_cero@hotmail.com>
2020-03-03 17:36:42 +0000

    New trailer form created

--- app/controllers/trailers_controller.rb
+++ app/controllers/trailers_controller.rb
@@ -1,37 +1,39 @@
 class TrailersController < ApplicationController
 
-  # GET: /trailers
   get "/trailers" do
-    erb :"/trailers/index"
+    erb :"/trailers/index" 
   end
 
-  # GET: /trailers/new
+
   get "/trailers/new" do
     erb :"/trailers/new"
   end
 
-  # POST: /trailers
+ 
   post "/trailers" do
-    redirect "/trailers"
+    trailer = trailer.new(params[:trailer])
+    if trailer.save
+      redirect "/trailers/#{trailer_number}"
+    else
+      
+    end
+    
   end
 
-  # GET: /trailers/5
+  
   get "/trailers/:trailer_number" do
     @trailer = Trailer.find_by_trailer_number(params[:trailer_number])
     erb :"/trailers/show"
   end
 
-  # GET: /trailers/5/edit
-  get "/trailers/:trailer_number/edit" do
+    get "/trailers/:trailer_number/edit" do
     erb :"/trailers/edit"
   end
 
-  # PATCH: /trailers/5
   patch "/trailers/:trailer_number" do
     redirect "/trailers/:id"
   end
 
-  # DELETE: /trailers/5/delete
   delete "/trailers/:trailer_number/delete" do
     redirect "/trailers"
   end
