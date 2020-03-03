407406c57d744b9eab65792789edded0b329a8d4
reddevilcero <reddevil_cero@hotmail.com>
2020-03-03 10:34:53 +0000

    basic skeletor created

---
+++ app/models/helpers.rb
@@ -0,0 +1,17 @@
+class Helpers 
+
+  def self.params_empty?(params)
+    params.any? do |k,v|
+    v.empty?
+    end
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:id]
+  end
+
+  def self.current_user(session)
+    User.find_by_id(session[:id])
+  end 
+
+end
