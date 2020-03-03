25a4be9cab9d42dac2eb1a43a8c55127cc42ff80
reddevilcero <reddevil_cero@hotmail.com>
2020-03-03 15:22:17 +0000

    basic trailer show

--- app/controllers/trailers_controller.rb
+++ app/controllers/trailers_controller.rb
@@ -16,22 +16,23 @@ class TrailersController < ApplicationController
   end
 
   # GET: /trailers/5
-  get "/trailers/:id" do
+  get "/trailers/:trailer_number" do
+    @trailer = Trailer.find_by_trailer_number(params[:trailer_number])
     erb :"/trailers/show"
   end
 
   # GET: /trailers/5/edit
-  get "/trailers/:id/edit" do
+  get "/trailers/:trailer_number/edit" do
     erb :"/trailers/edit"
   end
 
   # PATCH: /trailers/5
-  patch "/trailers/:id" do
+  patch "/trailers/:trailer_number" do
     redirect "/trailers/:id"
   end
 
   # DELETE: /trailers/5/delete
-  delete "/trailers/:id/delete" do
+  delete "/trailers/:trailer_number/delete" do
     redirect "/trailers"
   end
 end
