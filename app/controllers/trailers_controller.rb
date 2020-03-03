6539236b17450f1ca216cc5f6ca8032c3929ca0d
reddevilcero <reddevil_cero@hotmail.com>
2020-03-03 19:32:19 +0000

    Edit and Delete created for Trailers

--- app/controllers/trailers_controller.rb
+++ app/controllers/trailers_controller.rb
@@ -22,7 +22,6 @@ class TrailersController < ApplicationController
     
   end
 
-  
   get "/trailers/:trailer_number" do
     @trailer = Trailer.find_by(trailer_number: params[:trailer_number])
     erb :"/trailers/show"
@@ -31,14 +30,26 @@ class TrailersController < ApplicationController
   get "/trailers/:trailer_number/edit" do
     @trailer = Trailer.find_by(trailer_number: params[:trailer_number]) 
 
+
     erb :"/trailers/edit"
   end
 
   patch "/trailers/:trailer_number" do
-    redirect "/trailers/:id"
+    trailer = Trailer.find_by(trailer_number: params[:trailer_number])
+    if trailer.update(params[:trailer])
+      flash[:success] ='Trailer Successfully Update'
+      redirect "/trailers/#{trailer.trailer_number}"
+    else
+      flash[:error] = trailer.errors.full_messages.first
+      redirect "/trailers/#{params[:trailer_number]}/edit"
+    end
+    
   end
 
   delete "/trailers/:trailer_number/delete" do
+    trailer = Trailer.find_by(trailer_number: params[:trailer_number])
+    trailer.destroy
+    flash[:warning] = "The Trailer #{params[:trailer_number]} has been delete"
     redirect "/trailers"
   end
 end
