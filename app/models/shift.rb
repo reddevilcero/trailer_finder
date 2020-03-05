e2c7b609c8a5e06fb9f4f18d6fa070b0269a35c3
reddevilcero <reddevil_cero@hotmail.com>
2020-03-05 13:05:01 +0000

    added relationship whit alias in shift

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,7 @@
 class Shift < ActiveRecord::Base 
   belongs_to :trailer
   belongs_to :worker
+  has_one :start_depot, :class_name => 'Depot', :foreign_key => 'id'
+  has_one :end_depot, :class_name => 'Depot', :foreign_key => 'id'
+
 end
