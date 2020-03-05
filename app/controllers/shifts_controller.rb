0131be79e7fa2ef9da64f0307f9af914ab507a12
reddevilcero <reddevil_cero@hotmail.com>
2020-03-05 19:49:55 +0000

    finish the basic backend and RESTful routes Models need improvemnts Relations to improve

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -11,8 +11,7 @@ class ShiftsController < ApplicationController
   end
 
   get '/shifts/new' do # can i call it Start?
-    if is_logged_in?(session) && current_user(session).rol = 'driver'
-
+    if is_logged_in?(session) && current_user(session).rol == 'driver'
       erb :'/shifts/new'
     else
       status 403
@@ -20,22 +19,34 @@ class ShiftsController < ApplicationController
     end
   end
 
-  get '/shift/:id' do
-
+  get '/shifts/:id' do
+    if is_logged_in?(session) && current_user(session).rol == 'driver'
+      @shift = Shift.find_by_id(params[:id])
+      if @shift
+        erb :'/shifts/show'
+      else
+        status 404
+        erb :"errors/404"
+      end
+    else
+      status 403
+      erb :"errors/403"
+    end
   end
 
-  get '/shift/:id/edit' do
-
+  get '/shifts/:id/edit' do
+    @shift = Shift.find_by_id(params[:id])
+    erb :'shifts/edit'
   end
 
   post '/shifts' do
     #Better Way to do this?
-    binding.pry
-    if is_logged_in?(session) && current_user(session).rol = 'driver'
-      shift = Shift.new(params[:shift])
-      shift.start_shift = Depot.find_by(id:params[:start_depot])
-      shift.end_shift = Depot.find_by(id:params[:end_depot])
-
+    if is_logged_in?(session) && current_user(session).rol == 'driver'
+      shift = Shift.create(params[:shift])
+      # shift.start_depot= Depot.find_by(id:params[:start_depot])
+      # shift.end_depot = Depot.find_by(id:params[:end_depot])
+      # shift.save
+      redirect "/shifts/#{shift.id}"
     else
       status 403
       erb :"errors/403"
@@ -43,11 +54,19 @@ class ShiftsController < ApplicationController
 
   end
 
-  patch 'shifts/:id' do
+  patch '/shifts/:id' do
 
+    if is_logged_in?(session) && current_user(session).rol == 'driver'
+      shift = Shift.find_by_id(params[:id])
+      shift.update(params[:shift])
+      redirect "/shifts/#{shift.id}"
+    else
+      status 404
+      erb :"errors/404"
+    end
   end
 
-  delete 'shifts/:id/delete' do
+  delete '/shifts/:id/delete' do
     
   end
 
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,7 +2,11 @@ class Shift < ActiveRecord::Base
   belongs_to :trailer
   belongs_to :worker
   #better way to do this?
-  has_one :start_depot, :class_name => 'Depot', :foreign_key => 'id'
-  has_one :end_depot, :class_name => 'Depot', :foreign_key => 'id'
+  # has_one :start_depot, :class_name => 'Depot', :foreign_key => 'id'
+  # has_one :end_depot, :class_name => 'Depot', :foreign_key => 'id'
 
+
+  def incomplete?
+    !self.end_depot
+  end
 end
