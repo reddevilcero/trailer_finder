dd0ef083c2c73ae7874d92a3110539f49a662a3d
Morgan VanYperen <morgvanny@gmail.com>
2020-03-11 12:57:51 -0700

    fix db issues

--- db/migrate/20200311005210_rename_depot_keys.rb
+++
@@ -1,6 +0,0 @@
-class RenameDepotKeys < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :shifts, :start_depot, :start_depot_id
-    rename_column :shifts, :end_depot, :end_depot_id
-  end
-end
